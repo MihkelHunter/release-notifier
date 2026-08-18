18be27bd749041fab9feb46e34d72b97d308de54
MihkelHunter <mihkelkytt@gmail.com>
2026-08-18 09:07:24 +0300

    ups

--- internal/config/config.go
+++ internal/config/config.go
@@ -30,7 +30,7 @@ type EnvConfig struct {
 func DefaultConfig() *Config {
 	return &Config{
 		Environments: map[string]EnvConfig{
-			"production": {SubjectPrefix: "live'i läinud AX2012 uuendused"},
+			"production": {SubjectPrefix: "Live'i läinud AX2012 uuendused"},
 			"staging":    {SubjectPrefix: "[STAGING RELEASE]"},
 			"hotfix":     {SubjectPrefix: "🔥 [HOTFIX]"},
 		},
